262b96712729369d05ea72d85446bdbedf54cfa7
mlymarketplace-lgtm <mlymarketplace@gmail.com>
2026-07-04 17:44:02 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-// QualifGaïndé Worldwide — Service Worker V11.5.18
-const CACHE_VERSION = 'qg-v11-5-18-static';
-const RUNTIME_CACHE = 'qg-v11-5-18-runtime';
+// QualifGaïndé Worldwide — Service Worker V11.5.19
+const CACHE_VERSION = 'qg-v11-5-19-static';
+const RUNTIME_CACHE = 'qg-v11-5-19-runtime';
 const CORE_ASSETS = [
   '/',
   '/index.html',
