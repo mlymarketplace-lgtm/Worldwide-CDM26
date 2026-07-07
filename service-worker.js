a4991e9cf51f78528a7a89d06b7089dabe97e313
mlymarketplace-lgtm <mlymarketplace@gmail.com>
2026-07-07 10:49:20 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-// QualifGaïndé Worldwide — Service Worker V12.0.13 SAFE
-const CACHE_VERSION = 'qg-v12-0-13-static';
-const RUNTIME_CACHE = 'qg-v12-0-13-runtime';
+// QualifGaïndé Worldwide — Service Worker V13.0.5 SAFE
+const CACHE_VERSION = 'qg-v13-0-5-static';
+const RUNTIME_CACHE = 'qg-v13-0-5-runtime';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
